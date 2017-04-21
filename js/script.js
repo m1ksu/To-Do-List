79f7346a3c58da5f3fa70051f7f879b572534a3d
m1ksu <mikael.stenman112@gmail.com>
2017-04-21 21:35:41 +0300

    Added local storage integration.

--- js/script.js
+++ js/script.js
@@ -1,38 +1,37 @@
 $(function(){
 
     function inputVal() {
-        $('ul').append('<li>' +
-        $('input').val() + removeButton
-        + '</li>');
+        $("ul").append("<li>" +
+        $("input").val() + removeButton
+        + "</li>");
 
-        $('#listput').val('');
+        $("#listput").val("");
     }
 
     // Pressing the button inputs the item.
-    $('.button').on('click', function() {
+    $(".button").on("click", function() {
         inputVal();
     });
 
     // Pressing enter inputs the item.
-    $('#listput').on('keypress', function(e) {
+    $("#listput").on("keypress", function(e) {
         if (e.which === 13) {
             inputVal();
         }
     });
 
     // Remove list item on click.
-    $(document).on('click', '#remove', function() {
+    $(document).on("click", "#remove", function() {
         $(this).parent().remove();
     });
 
     // Remove button's HTML..
-    var removeButton = '<span id="remove"> x </span>'
+    var removeButton = "<span id=\"remove\"> x </span>"
 
 
-    $("#save").on('click', function() {
+    $("#save").on("click", function() {
 
         var listHTML;
-
 
         for (i = -1; i <= $("ul > *").length - 2; i++) {
             listHTML += $(`ul > *:eq(${i})`).html();
@@ -43,7 +42,7 @@ $(function(){
         if (typeof(Storage) !== "undefined") {
             console.log(1);
         } else {
-            console.log("No support for web storage.");
+            $("#stor-unav").css("visibility", "hidden");
         }
     });
 });

bacde882efc4d8c0ece7d74463a40ee63b5083ef
m1ksu <mikael.stenman112@gmail.com>
2017-04-22 21:50:48 +0300

    Indented Gruntfile properly.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,50 +1,50 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    pug: {
-        compile: {
-            options: {
-                pretty: false
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        pug: {
+            compile: {
+                options: {
+                    pretty: false
+                },
+
+                files: {
+                    'build/index.html': ['src/index.pug']
+                }
+            }
+        },
+
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed',
+                    sourcemap: 'none'
+                },
+                files: {
+                    'build/style.css': 'src/style.sass'
+                }
+            }
+        },
+
+        watch: {
+            pug: {
+                files: ['src/index.pug'],
+                tasks: ['pug']
             },
-            
-            files: {
-                'build/index.html': ['src/index.pug']
+            sass: {
+                files: ['src/style.sass'],
+                tasks: ['sass']
             }
         }
-    },
+    });
 
-    sass: {
-        dist: {
-            options: {
-                style: 'compressed',
-                sourcemap: 'none'
-            },
-			files: {
-				'build/style.css': 'src/style.sass'
-			}
-		}
-    },
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-pug');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-    watch: {
-        pug: {
-            files: ['src/index.pug'],
-            tasks: ['pug']
-        },
-        sass: {
-            files: ['src/style.sass'],
-            tasks: ['sass']
-        }         
-    }
-  });
-
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-pug');
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-
-  // Default task(s).
-  grunt.registerTask('default', ['watch']);
+    // Default task(s).
+    grunt.registerTask('default', ['watch']);
 
 };
